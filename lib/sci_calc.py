82ced29fa55a0d284a521f5b98d077b4cd26f193
Christopher Walsh <walshification@gmail.com>
2016-01-03 09:45:14 -0600

    adds scientific calculator skeleton with arithmetic defined

---
+++ lib/sci_calc.py
@@ -0,0 +1,13 @@
+class Arithmetic(object):
+    
+    def add(self, i, j):
+        return i + j
+
+    def subtract(self, i, j):
+        return i - j
+    
+    def multiply(self, i, j):
+        return i * j
+
+    def to_power(self, i, j):
+        return i**j
---
+++ tests/test_sci_calc.py
@@ -0,0 +1,74 @@
+import unittest
+
+from lib import sci_calc
+
+
+class TestArithmetic(unittest.TestCase):
+    def setUp(self):
+        self.calc = sci_calc.Arithmetic()
+
+    def test_addition(self):
+        result = self.calc.add(3, 4)
+        self.assertEqual(result, 7)
+
+    def test_subtraction(self):
+        result = self.calc.subtract(3, 4)
+        self.assertEqual(result, -1)
+
+    def test_multiply(self):
+        result = self.calc.multiply(3, 4)
+        self.assertEqual(result, 12)
+
+    def test_exponents(self):
+        result = self.calc.to_power(3, 4)
+        self.assertEqual(result, 81)
+
+
+class TestTrigonometry(unittest.TestCase):
+    def test_logarithmic_operations(self):
+        pass
+
+    def test_trigonometry(self):
+        pass
+
+
+class TestAltMaths(unittest.TestCase):
+    def test_hexadecimal(self):
+        pass
+
+    def test_octal(self):
+        pass
+
+    def test_Boolean_math(self):
+        pass
+
+    def test_complex_numbers(self):
+        pass
+
+    def test_scientific_notation(self):
+        pass
+
+    def test_fractions(self):
+        pass
+
+
+class TestStats(unittest.TestCase):
+    def test_statistics(self):
+        pass
+
+    def test_probability(self):
+        pass
+
+
+    def test_matrix_calculations(self):
+        pass
+
+    def test_calculus(self):
+        pass
+
+    def test_physical_constants(self):
+        pass
+
+class TestUnitConversion(unittest.TestCase):
+    def test_unit_conversion(self):
+        pass
